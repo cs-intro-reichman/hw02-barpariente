aa7e46cc4d597f06f0f59dcc199aa78549dc2897
barpariente <154364507+barpariente@users.noreply.github.com>
2023-12-31 20:58:28 +0200

    Add files via upload

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -1,11 +1,58 @@
-/**
- *  Computes some statistics about families in which the parents decide 
- *  to have children until they have at least one child of each gender.
- *  The program expects to get one command-line argument: an int value
- * 	that determines how many families to simulate.
- */
 public class OneOfEachStats1 {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        
+        int T = Integer.parseInt(args[0]); 
+        int families2 = 0; 
+        int families3 = 0; 
+        int families4 = 0; 
+        int child = 0; 
+        int common = 0; 
+        int avg = 1; 
+        
+        for (int i =0; i<T; i++) {
+
+            child = 1; 
+
+            int first = (int)((Math.random()*2));
+            int gender = first;
+
+                while (gender == first) {
+
+                    gender = (int)((Math.random()*2));
+                    
+                    child ++; 
+                 }
+            if (child == 2){
+                families2 ++;
+            }     
+            else if (child == 3){
+                families3 ++; 
+            }
+            else {
+                families4 ++; 
+            }
+            avg = avg + child; 
+
+}
+        double avarege = ((double)(avg) / (double) (T)); 
+        System.out.println("Average: " + avarege + " children to get at least one of each gender");
+        System.out.println("Number of families with 2 children: " + families2);
+        System.out.println("Number of families with 3 children: " + families3);
+        System.out.println("Number of families with 4 or more children: " + families4);   
+
+        if ((families2 > families3)&&(families2 > families4)) {
+
+            System.out.println("The most common number of children is 2"); 
+        }
+        else if ((families3 > families2)&&(families3 > families4)) {
+
+            System.out.println("The most common number of children is 3"); 
+ 
+        }
+        else {
+
+            System.out.println("The most common number of children is 4 or more");
+        }
+        
+    }    
 }
