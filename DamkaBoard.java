0187a1f06eb5387bf48d552978f1ca3fb49b1bb0
barpariente <154364507+barpariente@users.noreply.github.com>
2024-01-01 09:56:29 +0200

    Add files via upload

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,14 +3,18 @@ public class DamkaBoard {
 
         int a = Integer.parseInt(args[0]);
         String rows = ""; 
+        String rvs = ""; 
         
-        for (int i = 0; i<a; i++ ){
-            rows = rows + "* " ; 
+        for (int i = 1; i<=a; i++ ){
+            
+            rows = rows + "* " ;
+            rvs = rvs + " *"; 
 
         }
         for (int i = 1; i <= a; i++ ){
             if ((i%2) == 0){
-                System.out.println(" " + rows);
+
+                System.out.println(rvs);
             }
             else {
                 System.out.println(rows);
